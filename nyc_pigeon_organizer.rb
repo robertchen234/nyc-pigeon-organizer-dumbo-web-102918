eccba3a697033e86a7d92076d5308089adb56f87
Robert Chen <robertchen234@gmail.com>
2018-10-23 02:42:54 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,62 +2,80 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   
   new_hash = {}
-  
+
+# Get array of unique names  
   pigeon_names = data.collect do |attributes, details|
     details.values 
   end 
   .flatten.uniq 
-  
+
+# Create hash with each name as key
+# {"Theo"=> {}, "Peter" => {},...}  
   pigeon_names.each do |name|
     new_hash[name] = []
   end 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-# Get array of unique names
-  pigeon_names = data.collect do |attributes, details|
-    details.values
-    end
-    .flatten.uniq
-  
-# Create hash with each name as key
-# {"Theo"=> {}, "Peter" => {},...}
-  pigeon_names.each do |name| 
-    new_hash[name] = {} 
-  end
-  
-# Create keys(categories for each pigeon) with value of empty array
+
+# Create keys(categories for each pigeon) with value of empty array  
   data.each do |attributes, details|
     new_hash.each do |name, hash|
       new_hash[name][attributes] = []
     end
-  end
-  
-# Assign info to each key
-  data.each do |attribute, details|
+  end  
+
+# Assign info to each key  
+  data.each do |attributes, details|
     details.each do |property, names|
       names.each do |name|
-        new_hash[name][attribute] << property.to_s
-      end
-    end
-  end
+        new_hash[name][attributes] << property.to_s 
+      end 
+    end 
+  end 
   
-  new_hash
+  new_hash 
   
 end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+#   pigeon_names = data.collect do |attributes, details|
+#     details.values
+#     end
+#     .flatten.uniq
+  
+
+#   pigeon_names.each do |name| 
+#     new_hash[name] = {} 
+#   end
+  
+
+#   data.each do |attributes, details|
+#     new_hash.each do |name, hash|
+#       new_hash[name][attributes] = []
+#     end
+#   end
+  
+
+#   data.each do |attribute, details|
+#     details.each do |property, names|
+#       names.each do |name|
+#         new_hash[name][attribute] << property.to_s
+#       end
+#     end
+#   end
+  
+#   new_hash
+  
+# end
