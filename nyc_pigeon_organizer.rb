401693479a8d8483c22818845e0fc7ab418b5642
Robert Chen <robertchen234@gmail.com>
2018-10-23 02:44:51 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -34,34 +34,3 @@ def nyc_pigeon_organizer(data)
   new_hash 
   
 end
-  
-
-#   pigeon_names = data.collect do |attributes, details|
-#     details.values
-#     end
-#     .flatten.uniq
-  
-
-#   pigeon_names.each do |name| 
-#     new_hash[name] = {} 
-#   end
-  
-
-#   data.each do |attributes, details|
-#     new_hash.each do |name, hash|
-#       new_hash[name][attributes] = []
-#     end
-#   end
-  
-
-#   data.each do |attribute, details|
-#     details.each do |property, names|
-#       names.each do |name|
-#         new_hash[name][attribute] << property.to_s
-#       end
-#     end
-#   end
-  
-#   new_hash
-  
-# end
