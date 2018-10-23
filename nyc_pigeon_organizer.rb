8621e18b772c45d1ec08ebccd7a506ae5b58580f
Robert Chen <robertchen234@gmail.com>
2018-10-23 01:31:44 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,15 @@
 def nyc_pigeon_organizer(data)
   new = {}
   data.each do |infos| #blocks
+  binding.pry
     infos.each do |info| #color and gender
+  binding.pry
       info.each do |attribute, details| #colorGenderLives, colorMaleFemaleSubwayCentralparkLibraryCityhall => names
+  binding.pry
         details.each do |detail, names| #colorMaleFemaleSubwayCentralparkLibraryCityhall, array of names
+  binding.pry
           names.each do |name| #string from array
+  binding.pry
             new[:name] = {
               :color => detail if attribute.include?("color")
               :gender => detail if attribute.include?("gender")
