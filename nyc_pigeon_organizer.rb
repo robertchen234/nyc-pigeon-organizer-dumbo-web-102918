cacaac10456766346001b3c26aa2ef73466b211c
Robert Chen <robertchen234@gmail.com>
2018-10-23 01:36:37 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,33 @@
 require 'pry'
 
+def nyc_pigeon_organizer(data)
+  # write your code here!	  # Create a hash for results
+end 	  new_hash = {}
+   # Get array of unique names
+  pigeon_names = data.collect do |attributes, details|
+    details.values
+  end
+  .flatten.uniq
+   # Create hash with each name as key
+  # {"Theo"=> {}, "Peter" => {},...}
+  pigeon_names.each {|name| new_hash[name] = {}}
+   # Create keys(categories for each pigeon) with value of empty array
+  data.each do |attributes, details|
+    new_hash.each do |name, hash|
+      new_hash[name][attributes] = []
+    end
+  end
+   # Assign info to each key
+  data.each do |attribute, details|
+    details.each do |property, names|
+      names.each do |name|
+        new_hash[name][attribute] << property.to_s
+      end
+    end
+  end
+  new_hash
+end
+
 # def nyc_pigeon_organizer(data)
 #   new = {}
 #   data.each do |infos| #blocks
