d87530100f2d4dffbfa2033089cb853f35d71fce
Robert Chen <robertchen234@gmail.com>
2018-10-23 01:07:47 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each do |names|
+    names.each do |name, infos|
+      infos.each do |info, detail|
+        
+      end
+    end 
+  end 
+    
 end
