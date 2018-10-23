0f886d48642c25880b1a68ebc62c271ed24f11e5
Robert Chen <robertchen234@gmail.com>
2018-10-23 01:36:24 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,28 +1,28 @@
 require 'pry'
 
-def nyc_pigeon_organizer(data)
-  new = {}
-  data.each do |infos| #blocks
-  binding.pry
-    infos.each do |info| #color and gender
-  binding.pry
-      info.each do |attribute, details| #colorGenderLives, colorMaleFemaleSubwayCentralparkLibraryCityhall => names
-  binding.pry
-        details.each do |detail, names| #colorMaleFemaleSubwayCentralparkLibraryCityhall, array of names
-  binding.pry
-          names.each do |name| #string from array
-  binding.pry
-            new[:name] = {
-              :color => detail if attribute.include?("color")
-              :gender => detail if attribute.include?("gender")
-              :lives => detail if attribute.include?("lives")
-            }
-          end
-        end 
-      end
-    end 
-  end 
-  new
-end
+# def nyc_pigeon_organizer(data)
+#   new = {}
+#   data.each do |infos| #blocks
+#   binding.pry
+#     infos.each do |info| #color and gender
+#   binding.pry
+#       info.each do |attribute, details| #colorGenderLives, colorMaleFemaleSubwayCentralparkLibraryCityhall => names
+#   binding.pry
+#         details.each do |detail, names| #colorMaleFemaleSubwayCentralparkLibraryCityhall, array of names
+#   binding.pry
+#           names.each do |name| #string from array
+#   binding.pry
+#             new[:name] = {
+#               :color => detail if attribute.include?("color")
+#               :gender => detail if attribute.include?("gender")
+#               :lives => detail if attribute.include?("lives")
+#             }
+#           end
+#         end 
+#       end
+#     end 
+#   end 
+#   new
+# end
 
-nyc_pigeon_organizer("Pigeons")
+# nyc_pigeon_organizer("Pigeons")
